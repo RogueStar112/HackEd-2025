a2a0b734df96bb131a0621a7494a9fe0384dfc94
DMistica <hildtgunnt@gmail.com>
2025-09-18 10:10:45 +0100

    SEO Improvement

--- hatch/app/layout.tsx
+++ hatch/app/layout.tsx
@@ -9,8 +9,8 @@ const defaultUrl = process.env.VERCEL_URL
 
 export const metadata: Metadata = {
   metadataBase: new URL(defaultUrl),
-  title: "Next.js and Supabase Starter Kit",
-  description: "The fastest way to build apps with Next.js and Supabase",
+  title: "Hatch - Where Collaboration Is Born",
+  description: "Weave your way with work experience with Hatch!",
 };
 
 const geistSans = Geist({
